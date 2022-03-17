60710ecce7fe22aede8f166eef602d860a053a4b
Your Name <test@gmail.com>
2022-03-17 14:27:09 +0530

    commit

--- March-Assignment/16-march assignment/index.js
+++ March-Assignment/16-march assignment/index.js
@@ -1,5 +1,36 @@
+// Addition
  function sum (n1 ,n2 ){
      add = n1+n2
      console.log(add);
  }
  sum(10,20);
+
+
+
+
+ //  Subtracton
+function Subtracton (n1,n2){
+    min = n1-n2
+    console.log(min);
+}
+Subtracton(45,30);
+
+
+
+// multiplication
+function multiplication(n1,n2,n3){
+    mult = n1*n2*n3
+    console.log(mult);
+
+}
+multiplication(3,2,10);
+
+
+
+// division
+function division(n1,n2){
+    div = n1/n2
+     console.log(div);
+
+}
+division(44,11);
